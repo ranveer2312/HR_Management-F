395697f7be6d1e5aa629b5738ad86313d4777460
Rana Ranveer Kumar Yadav <ranveer30654@gmail.com>
2025-08-26 09:58:36 +0530

    url

--- src/app/employee/layout.tsx
+++ src/app/employee/layout.tsx
@@ -42,7 +42,7 @@ interface AttendanceRecord {
   workHours: number;
 }
 
-const APIURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+const APIURL = process.env.NEXT_PUBLIC_API_URL || 'https://hr-management-b.onrender.com';
 
 type NavItemProps = { icon: React.ReactNode; label: string; href?: string; active?: boolean; hasNotification?: boolean };
 const NavItem = ({ icon, label, href = "#", active = false, hasNotification = false }: NavItemProps) => (
